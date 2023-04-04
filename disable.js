9baf497450dab7c78461937ccb11ec67e27e09f1
আলি হাসান <101947194+dev-alihasan@users.noreply.github.com>
2023-04-04 22:58:32 +0600

    Create disable.js

---
+++ disable.js
@@ -0,0 +1,9 @@
+// Disable right-click context menu
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Disable F12 key and Ctrl+Shift+I combo
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 123 || (event.ctrlKey && event.shiftKey && event.keyCode === 73)) {
+    event.preventDefault();
+  }
+});
